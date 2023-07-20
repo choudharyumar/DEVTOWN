54627ed0b272ba5abcaa91a147d27ee56dee4207
choudharyumar <choudharyumar948@gmail.com>
2023-07-20 17:03:27 +0500

    Update t1.js

--- t1.js
+++ t1.js
@@ -1,3 +1,10 @@
-let a="hi";
-console.log(a)
+// function greeting(age)
+// {
+//     let name=prompt("what is your name?");
+// alert("hi "+name+", welcome to layyah and your age is "+age)    
+// return age;
+// }
+// greeting(23);
+let a='ali';
+console.log(a);
 
